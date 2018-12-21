6442e0fcb2f53036fee5bc952936b773f09907ac
= <=>
2018-12-21 15:13:21 +0100

    TP html

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var arr = ["This is the first element", "This is the second element"];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[arr.length - 1]);
+
+var fish = ["Lion", "Angel"];
+console.log(fish[0]);
+console.log(fish[1]);
+console.log(fish[2]);
